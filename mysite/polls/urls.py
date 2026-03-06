26974adbc86ba5c74dde3296ea4e6dcab2380869
nisargamd005-ai <nisargamd005@gmail.com>
2026-03-06 20:48:28 +0530

    jbec

--- mysite/polls/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'polls'
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('<int:question_id>/vote/', views.vote, name='vote'),
-]
